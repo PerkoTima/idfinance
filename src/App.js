e17b8a5e3aa2c87a6efa113f5a11fb95c98e4087
Perko Tima <perko.t@mail.ru>
2022-09-28 13:59:49 +0300

    fix fetch

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.scss';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Content from './components/Wrapper/Wrapper';
-import { useState } from 'react';
+// import { useState } from 'react';
 import schema from "./schema/schema.json"
 
 function App() {
-  const [data, setData] = useState()
+  // const [data, setData] = useState()
   console.log(schema)
   
   return (
